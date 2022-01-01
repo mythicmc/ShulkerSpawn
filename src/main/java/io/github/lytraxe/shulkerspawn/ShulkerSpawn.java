59f22898a4d3476fc3a66fe6ffbc5980d856a747
Lytraxe <36500968+Lytraxe@users.noreply.github.com>
2022-01-01 11:42:59 +0530

    Added '/shulkerspawn set' command

--- src/main/java/io/github/lytraxe/shulkerspawn/ShulkerSpawn.java
+++ src/main/java/io/github/lytraxe/shulkerspawn/ShulkerSpawn.java
@@ -32,7 +32,7 @@ public class ShulkerSpawn extends JavaPlugin implements Listener {
     //- Original Author : https://github.com/JoelGodOfwar
     //- https://github.com/JoelGodOfwar/ShulkerRespawner/blob/master/1.13/src/com/github/joelgodofwar/sr/ShulkerRespawner.java
     //-
-    
+
     @EventHandler
     public void onCreatureSpawn(CreatureSpawnEvent event) {
         if (event.getEntity() instanceof Enderman && event.getLocation().getWorld().getEnvironment() == World.Environment.THE_END
@@ -61,15 +61,40 @@ public class ShulkerSpawn extends JavaPlugin implements Listener {
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (args.length == 1 && args[0].equalsIgnoreCase("reload") && sender.hasPermission("shulkerspawn.admin")) {
-            readConfig();
-            sender.sendMessage(ChatColor.GRAY + "Reloaded plugin configuration");
-            return true;
-        } else if (args.length == 0) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8---------------[ &3" + getDescription().getName() + " &fv" + getDescription().getVersion() + " &8]---------------"));
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', " &7- Current Shulker SpawnRate: &r" + spawnChance + "%"));
-            return true;
+        if (sender.hasPermission("shulkerspawn.admin")) {
+            if (args.length == 0) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8---------------[ &3" + getDescription().getName() + " &fv" + getDescription().getVersion() + " &8]---------------"));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', " &7- Current Shulker SpawnRate: &r" + spawnChance + "%"));
+                return true;
+            }
+            //- Reload command
+            if (args[0].equalsIgnoreCase("reload")) {
+                readConfig();
+                sender.sendMessage(ChatColor.GRAY + "Reloaded plugin configuration");
+                return true;
+            }
+            //- set config command (really unnecessary but okay)
+            else if (args[0].equalsIgnoreCase("set")) {
+                if (args.length == 3) {
+                    if (args[1].equalsIgnoreCase("SpawnChance")) {
+                        int chance;
+                        try {chance = Integer.parseInt(args[2]);} catch (NumberFormatException e) {sender.sendMessage(ChatColor.RED + "Value must be an integer (0-100)"); return true;}
+                        if (chance >= 0 && chance <= 100) {
+                            getConfig().set("SpawnChance", chance);
+                            saveConfig();
+                            readConfig();
+                            sender.sendMessage(ChatColor.GRAY + " done :>");
+                            return true;
+                        }
+                        sender.sendMessage(ChatColor.GRAY + "Value of SpawnChance must be an Integer between 0 and 100"); return true;
+                    }
+                    sender.sendMessage(ChatColor.GRAY + "Available params = 'SpawnChance'"); return true;
+                }
+                sender.sendMessage(ChatColor.GRAY + "Usage: " + ChatColor.WHITE + "/shulkerspawn set <param> <value>"); return true;
+            }
+            return false;
         }
-        return false;
+        sender.sendMessage(ChatColor.RED + "You do not have the permission to perform this action");
+        return true;
     }
 }
